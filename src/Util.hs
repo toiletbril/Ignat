83d08c9876796be8c458e1cba7b8f0edc1b22c07
toiletbril <s9und@outlook.com>
2023-04-07 18:47:07 +0400

    Implement headMay

--- src/Util.hs
+++ src/Util.hs
@@ -3,6 +3,8 @@
 module Util where
 
 import           App
+import           Control.Monad
+import           Data.Maybe    (fromMaybe)
 import           Data.Text     as T
 import           Data.Text.IO  as T (putStrLn)
 import           Discord.Types
@@ -15,7 +17,14 @@ packShow :: Show a => a -> Text
 packShow = T.pack . show
 
 commandType :: Text -> Text
-commandType = T.tail . Prelude.head . T.words
+commandType x = fromMaybe "" $ t <=< headMay $ T.words x
+  where
+    t "" = Nothing
+    t a  = Just (T.tail a)
+
+headMay :: [a] -> Maybe a
+headMay [] = Nothing
+headMay a  = Just (Prelude.head a)
 
 startsWith :: Message -> Text -> Bool
 startsWith = isPrefixOf . messageContent
